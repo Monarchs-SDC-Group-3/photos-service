2f24fb17d956cef4fad63e2d085b1549c7de6418
Alex Yi <alexyi164@gmail.com>
2020-11-11 17:48:15 -0800

    Initial boilerplate commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+const PUB_DIR = path.resolve(__dirname, "..", "public");
+
+app.use(express.static(PUB_DIR));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`running on ${port}`);
+  }
+});
