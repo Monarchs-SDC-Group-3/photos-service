75686b1456eef890d744bccdb76c1da1ef24e522
Alex Yi <alexyi164@gmail.com>
2020-11-16 12:10:52 -0800

    successfully merged after struggle

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   presets: [
+//     [
+//       '@babel/preset-react',
+//       {
+//         targets: {
+//           node: 'current',
+//         },
+//       },
+//     ],
+//     '@babel/preset-env',
+//   ],
+// };
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
---
+++ client/components/photos.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Photos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  getPhotos() {
+
+  }
+
+  render() {
+    return (
+      <div>THIS IS WORKING NOW</div>
+    )
+  }
+}
+
+export default Photos;
